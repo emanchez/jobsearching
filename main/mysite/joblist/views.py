a5c532fe9f2e71993c838dc735124c807e037bd7
Emmanuel Sanchez <sanchez.emmanuelm@gmail.com>
2023-08-17 09:51:36 -0400

    modified:   main/mysite/joblist/__pycache__/views.cpython-310.pyc 	modified:   main/mysite/joblist/templates/joblist/index.html 	modified:   main/mysite/joblist/views.py 	modified:   main/mysite/static/joblist/css/index.css
    added pagination in django python (views.py)
    and django-templates (index.html)

--- main/mysite/joblist/views.py
+++ main/mysite/joblist/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from joblist.models import JobListing
 from django.template import loader
@@ -12,9 +13,17 @@ def error(request):
     return HttpResponse("404 page not found")
 
 def index(request):
-    latestListing = JobListing.objects.all()[:10]
+    latestListing = JobListing.objects.all()[:100]
+    paginator = Paginator(latestListing, 9) 
     template = loader.get_template("joblist/index.html")
+    
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        "latest_job_listings": latestListing,
+        "latest_job_listings": page_obj,
     }
     return HttpResponse(template.render(context, request))
+
+
+# mostly followed tutorial on django docs "https://docs.djangoproject.com/en/4.2/intro/tutorial01/"
+# reference for django pagination: "https://docs.djangoproject.com/en/4.2/topics/pagination/"
